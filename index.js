f11c7142b6b58979259a2e2c16cce9ddc2bb7ceb
vvckovv <oloyedeope14@gmail.com>
2024-08-16 11:17:07 +0100

    added and edited to html and css and javascript

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-// const email = document.getElementById("email");
-// const password = document.getElementById("password");
-// const dob = document.getElementById("dob");
-// const options = document.getElementById("options");
-
-// function submit(){
-//     if(email.value === '/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/'){
-//     }
-//     else {
-//         let group = document.createElement("li");
-//     }
-// }   
-
-function validateForm() {
-    let submit = document.forms["myForm"]["fname"].value;
-    if (submit == "") {
-      alert("Name must be filled out");
-      return false;
-    }
+const email = document.getElementById("email");
+function validateEmail(email) {
+  let re = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
+  return re.test(email);
 }
+    
+console.log(validateEmail('Tunmise@anystring.any'));
+    
+console.log(validateEmail('my email is anystring@anystring .any'));
+
+const emailInp = document.querySelector('#email');
+const passwordInp = document.querySelector('#password');
+const date = document.querySelector('#date');
+const options = document.querySelector('#options');
+const result = document.querySelector('.result');
+const form = document.querySelector('#form');
+const errMsg = document.querySelector('.err-msg')
+let res = []
+function submit() {
+  res = [];
+  let emailPattern = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
+  if (!emailPattern.test(emailInp.value)){
+      errMsg.innerHTML = 'input correct email';
+      errMsg.style.color = 'red';
+      errMsg.style.marginTop = '-14px'
+  } else {
+      if(emailInp.value){
+          res.push(`"email": "${emailInp.value}"`)
+          errMsg.innerHTML = '';
+          errMsg.style.color = ''
+          errMsg.style.marginTop = ''
+      } 
+      if(passwordInp.value){
+          res.push(`"password": "${passwordInp.value}"`) 
+      }
+      if(date.value){
+          res.push(`"date": "${date.value}"`) 
+      };
+      if(options.value){
+          res.push(`"password": ["${options.value}"]`) 
+      }
+  }
+  result.innerHTML = `{${res}}`
+}
+
+function reset(){
+  emailInp.value = '';
+  passwordInp.value = '';
+  date.value = '';
+  options.value = ''   
+};
